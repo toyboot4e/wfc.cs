57fb58a21683a5ee67e7c7eefff0b62953822e48
toyboot4e <toyboot4e@gmail.com>
2019-12-25 00:35:26 +0900

    add Example/Main.cs

---
+++ Example/Main.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using Wfc;
+
+namespace Wfc.Example {
+    class Program {
+        static void Main(string[] args) {
+            var wfc = new WfcInput() {
+                mapFile = "Example/Res/a.txt",
+                    inputSize = new Vec2(6, 6),
+                    outputSize = new Vec2(30, 30),
+                    N = 3,
+            }.createModel();
+            // Console.WriteLine("======================");
+            // wfc.patterns.print();
+            // Console.WriteLine("======================");
+            wfc.run();
+            wfc.output.print();
+
+            // var another = new WfcInput() {
+            //     mapFile = "Res/b.txt",
+            //         inputSize = new Vec2(3, 3),
+            //         outputSize = new Vec2(30, 30),
+            //         N = 3,
+            // }.create();
+            // another.run();
+            // another.output.print();
+        }
+    }
+
+    class WfcInput {
+        public string mapFile;
+        public Vec2 inputSize;
+        public Vec2 outputSize;
+        public int N;
+
+        public Overlap.Model createModel() {
+            string asciiMap = File.ReadAllText(this.mapFile);
+            var nl = Environment.NewLine;
+            Console.WriteLine($@"Source map:{nl}{asciiMap}{nl}");
+
+            var map = MapExt.fromString(asciiMap, this.inputSize.x, this.inputSize.y);
+            return new Overlap.Model(map, this.N, this.outputSize.x, this.outputSize.y);
+        }
+    }
+}
---
+++ Lib/Utils/Print.cs
@@ -0,0 +1,93 @@
+using System;
+using System.IO;
+
+namespace Wfc {
+    public static class TileExt {
+        public static char toChar(this Tile self) {
+            switch (self) {
+                case Tile.None:
+                    return ' ';
+                case Tile.Wall:
+                    return '#';
+                case Tile.Floor:
+                    return '.';
+                case Tile.DownStair:
+                    return '>';
+                case Tile.UpStair:
+                    return '<';
+                default:
+                    throw new System.Exception("error in Tile.toChar()");
+            }
+        }
+
+        public static Tile charToTile(char c) {
+            switch (c) {
+                case ' ':
+                    return Tile.None;
+                case '#':
+                    return Tile.Wall;
+                case '.':
+                    return Tile.Floor;
+                case '>':
+                    return Tile.DownStair;
+                case '<':
+                    return Tile.UpStair;
+                default:
+                    throw new System.Exception("error in TileExt.charToTile()");
+            }
+        }
+    }
+
+    public static class MapExt {
+        public static Map fromString(string asciiMap, int width, int height) {
+            var self = new Map(width, height);
+
+            using(StringReader reader = new StringReader(asciiMap)) {
+                string buf;
+                int y = -1;
+                while ((buf = reader.ReadLine()) != null) {
+                    y += 1;
+                    for (int x = 0; x < width; x++) {
+                        self[x, y] = TileExt.charToTile(buf[x]);
+                    }
+                }
+            }
+
+            return self;
+        }
+
+        public static void print(this Map self) {
+            for (int y = 0; y < self.height; y++) {
+                for (int x = 0; x < self.width; x++) {
+                    Console.Write(self[x, y].toChar());
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public static class PatternExt {
+        public static void print(this Pattern self, Map source, int N) {
+            for (int j = 0; j < N; j++) {
+                for (int i = 0; i < N; i++) {
+                    var pos = self.offset + self.variant.apply(N, new Vec2(i, j));
+                    Console.Write(source[pos.x, pos.y].toChar());
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public static class PatternStorageExt {
+        public static void print(this PatternStorage self) {
+            foreach(var pattern in self.buffer) {
+                pattern.print(self.source, self.N);
+                Console.WriteLine($"weight: {pattern.weight}");
+                Console.WriteLine();
+            }
+
+            var n = self.buffer.Count;
+            Console.WriteLine($" { n } pattterns found ");
+        }
+    }
+}
