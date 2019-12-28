6bc33985a8b7bdeaa271dd095e3cfcb7c800e5f9
toyboot4e <toyboot4e@gmail.com>
2019-12-29 00:07:04 +0900

    improve Array

--- Lib/Utils/Array.cs
+++ Lib/Utils/Array.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 
 namespace Wfc {
+    /// <remark><c>add</c> items before using it. Continuous with x index</summary>
     public struct Array2D<T> {
         public int width;
         public List<T> values;
@@ -10,19 +11,24 @@ namespace Wfc {
             this.values = new List<T>(w * h);
         }
 
-        int index(int x, int y) => x + y * this.width;
+        public int capacity => this.values.Capacity;
 
+        public int index(int x, int y) => x + y * this.width;
         public T get(int x, int y) {
             return this.values[this.index(x, y)];
         }
-
         public void set(int x, int y, T value) {
             this.values[this.index(x, y)] = value;
         }
-
         public void add(T value) => this.values.Add(value);
+
+        public T this[int x, int y] {
+            get => this.get(x, y);
+            set => this.set(x, y, value);
+        }
     }
 
+    /// <remark><c>add</c> items before using it. Continuous with z index (then x index)</summary>
     public struct Array3D<T> {
         public int nx;
         public int nz;
@@ -34,16 +40,20 @@ namespace Wfc {
             this.values = new List<T>(nx * ny * nz);
         }
 
-        int index(int x, int y, int z) => (x + y * this.nx) * this.nz + z;
+        public int capacity => this.values.Capacity;
 
+        public int index(int x, int y, int z) => (x + y * this.nx) * this.nz + z;
         public T get(int x, int y, int z) {
             return this.values[this.index(x, y, z)];
         }
-
         public void set(int x, int y, int z, T value) {
             this.values[this.index(x, y, z)] = value;
         }
-
         public void add(T value) => this.values.Add(value);
+
+        public T this[int x, int y, int z] {
+            get => this.get(x, y, z);
+            set => this.set(x, y, z, value);
+        }
     }
 }
