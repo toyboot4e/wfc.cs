930e70b24b383a66a6ab37f3f490ea5add804621
toyboot4e <toyboot4e@gmail.com>
2020-02-09 00:50:50 +0900

    rename OverlapModel.cs to OverlappingModel.cs

--- Lib/PostProcess/Segments.cs
+++ Lib/PostProcess/Segments.cs
@@ -4,11 +4,31 @@ using System;
 namespace Wfc.Segments {
     public class Circle {
         public static void print(ref Map map) {
-            var center = new Vec2i(map.width / 2, map.height / 2);
-            var r = Math.Min(center.x, center.y) - 0.5f;
+            // e.g. (3, 3) -> (1,1), (4, 4) -> (1.5, 1.5)
+            var center = ((map.width - 1) / 2.0, (map.height - 1) / 2.0);
+            var r = Math.Min(center.Item1, center.Item2) + 0.5;
             for (int y = 0; y < map.height; y++) {
                 for (int x = 0; x < map.width; x++) {
-                    var d = (new Vec2i(x, y) - center).distanceInt();
+                    var delta = (center.Item1 - x, center.Item2 - y);
+                    var d = Math.Sqrt(delta.Item1 * delta.Item1 + delta.Item2 * delta.Item2);
+                    if (d <= r) {
+                        System.Console.Write(map[x, y].toChar());
+                    } else {
+                        System.Console.Write(" ");
+                    }
+                }
+                System.Console.WriteLine("");
+            }
+        }
+
+        public static void printOdd(ref Map map) {
+            // e.g. (3, 3) -> (1,1), (4, 4) -> (1, 1), (5, 5) -> (2, 2)
+            var center = new Vec2i((map.width - 1) / 2, (map.height - 1) / 2);
+            var r = Math.Min(center.x, center.y) + 0.5;
+            System.Console.WriteLine($"{center}, {r}");
+            for (int y = 0; y < map.height; y++) {
+                for (int x = 0; x < map.width; x++) {
+                    var d = (new Vec2i(x, y) - center).distanceD();
                     if (d <= r) {
                         System.Console.Write(map[x, y].toChar());
                     } else {
